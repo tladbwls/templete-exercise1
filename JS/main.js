088f9283d001a14399adfafe981ef668787c9da9
tladbwls <zipte@naver.com>
2022-11-23 12:45:47 +0900

    1123plant

--- JS/main.js
+++ JS/main.js
@@ -18,8 +18,37 @@ const stickyHeader = () => {
 
 /*===============================*/
 
+/*================menu active on each section===============*/
+const sections = document.querySelectorAll("section");
+console.log(sections);
+const headerHeight = document.querySelector("header").offsetHeight;
+
+const activateNavMenu = () => {
+  const scrY = window.scrollY;
+  sections.forEach((section) => {
+    const secHeight = section.offsetHeight; //section 요소의 높이값
+    const secTop = section.offsetTop - headerHeight; // sectin 요소의 윗쪽 옵셋값 - 헤더의 높이
+    const secId = section.getAttribute("id"); //각 섹션의 아이디값 저장
+
+    if (scrY > secTop && scrY <= secTop + secHeight) {
+      // && 합성곱 : 양쪽 모두 true일 경우 true
+      // || 합성합 : 양쪽 중 하나만 true일 경우 true
+      document.querySelector(`.nav-${secId}`).classList.add("active");
+    } else {
+      document.querySelector(`.nav-${secId}`).classList.remove("active");
+    }
+  });
+
+  if (scrY === 0) {
+    document.querySelector(`.nav-home`).classList.add("active");
+  }
+};
+
+/*===============================*/
+
 window.addEventListener("scroll", () => {
   stickyHeader();
+  activateNavMenu();
 });
 
 /*================Header menu toggle===============*/
@@ -41,5 +70,39 @@ const toggleMenu = (event) => {
 };
 
 menu.addEventListener("click", toggleMenu);
+
+/*===============================*/
+
+/*================Question section accordion===============*/
+
+const accHeaders = document.querySelectorAll(".question-box");
+
+const activateAccordion = (e) => {
+  // console.log(e.currentTarget);
+  const $this = e.currentTarget;
+  $this.classList.toggle("active");
+};
+
+accHeaders.forEach((header) => {
+  header.addEventListener("click", activateAccordion);
+});
+
+/*===============================*/
+
+/*==============Element fade up effect(reveal)=================*/
+
+const sr = ScrollReveal({
+  reset: false,
+  duration: 1000,
+  origin: "bottom",
+  distance: "50px",
+});
+
+sr.reveal(".home-box");
+sr.reveal(".about-box");
+sr.reveal(".step-box", { interval: 200 });
+sr.reveal(".item", { interval: 200 });
+
+// sr.reveal(".home-box", { duration: 1000, origin: "bottom", distance: "50px" });
 
 /*===============================*/
